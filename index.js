368f7606ef78b6a69216f4390490a66d34396dd2
oferitz <ofer.webdev@gmail.com>
2015-08-22 19:23:36 +0300

    Initial commit

---
+++ index.js
@@ -0,0 +1,37 @@
+var adjectives = require('./dictionary/adjectives');
+var animals = require('./dictionary/animals');
+var professions = require('./dictionary/professions');
+
+function getRandom(limit) {
+  return Math.floor(Math.random() * limit)
+}
+
+module.exports.generate = function generate(opts) {
+  var _opts = opts || {};
+  var stupidname = '';
+
+  if(typeof _opts.the !== 'undefined' &&  _opts.the === true) {
+    stupidname += 'the ';
+  }
+
+  var allWords = animals.concat(professions);
+  stupidname += (adjectives[getRandom(adjectives.length)] + ' ' + allWords[getRandom(allWords.length)]).toLowerCase();
+
+  if(typeof _opts.camelize !== 'undefined' && _opts.camelize === true) {
+    stupidname = stupidname.replace(/(?:^|\s)\S/g, function(a) { return a.toUpperCase(); });
+  }
+
+  if(typeof _opts.separator !== 'undefined') {
+    if(_opts.separator === false) {
+      // trim spaces
+      stupidname = stupidname.split(' ').join('');
+    }
+    else {
+      stupidname = stupidname.split(' ').join(_opts.separator);
+    }
+
+  }
+
+  return stupidname;
+
+};
---
+++ test.js
@@ -0,0 +1,22 @@
+var assert = require('assert');
+var YANG = require('./index');
+
+var simple = YANG.generate();
+assert(simple.length, 'failed to generate');
+assert(typeof simple === 'string', 'not typeof string');
+
+var withThe = YANG.generate({the: true});
+assert.equal(withThe.split(' ')[0], 'the', 'failed to add "the"');
+
+var camelize = YANG.generate({camelize: true});
+var words = camelize.split(' ');
+words.forEach(function(word, idx) {
+  var chr = word.charAt(0);
+  assert(/[A-Z]|[\u0080-\u024F]/.test(chr) && chr === chr.toUpperCase(), 'not camelized');
+});
+
+var seperatorFalse = YANG.generate({separator: false});
+assert.equal(seperatorFalse.split(' ').length, 1, 'failed to trim spaces between words');
+
+var withSeperator = YANG.generate({separator: '-'});
+assert(withSeperator.split('-').length > 1, 'failed to custom separate between words');
